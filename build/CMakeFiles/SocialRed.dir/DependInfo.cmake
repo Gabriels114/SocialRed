ce1037bcf09dda76c28658e163602371461bea43
Gabriels114 <gabriels114@gmail.com>
2024-11-28 03:16:27 -0600

    Build ejecutable

---
+++ build/CMakeFiles/SocialRed.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/gabriels/CLionProjects/SocialRed/src/Menu.cpp" "CMakeFiles/SocialRed.dir/src/Menu.cpp.o" "gcc" "CMakeFiles/SocialRed.dir/src/Menu.cpp.o.d"
+  "/home/gabriels/CLionProjects/SocialRed/src/RedSocial.cpp" "CMakeFiles/SocialRed.dir/src/RedSocial.cpp.o" "gcc" "CMakeFiles/SocialRed.dir/src/RedSocial.cpp.o.d"
+  "/home/gabriels/CLionProjects/SocialRed/src/main.cpp" "CMakeFiles/SocialRed.dir/src/main.cpp.o" "gcc" "CMakeFiles/SocialRed.dir/src/main.cpp.o.d"
+  "/home/gabriels/CLionProjects/SocialRed/third_party/imgui.cpp" "CMakeFiles/SocialRed.dir/third_party/imgui.cpp.o" "gcc" "CMakeFiles/SocialRed.dir/third_party/imgui.cpp.o.d"
+  "/home/gabriels/CLionProjects/SocialRed/third_party/imgui_draw.cpp" "CMakeFiles/SocialRed.dir/third_party/imgui_draw.cpp.o" "gcc" "CMakeFiles/SocialRed.dir/third_party/imgui_draw.cpp.o.d"
+  "/home/gabriels/CLionProjects/SocialRed/third_party/imgui_impl_glfw.cpp" "CMakeFiles/SocialRed.dir/third_party/imgui_impl_glfw.cpp.o" "gcc" "CMakeFiles/SocialRed.dir/third_party/imgui_impl_glfw.cpp.o.d"
+  "/home/gabriels/CLionProjects/SocialRed/third_party/imgui_impl_opengl3.cpp" "CMakeFiles/SocialRed.dir/third_party/imgui_impl_opengl3.cpp.o" "gcc" "CMakeFiles/SocialRed.dir/third_party/imgui_impl_opengl3.cpp.o.d"
+  "/home/gabriels/CLionProjects/SocialRed/third_party/imgui_tables.cpp" "CMakeFiles/SocialRed.dir/third_party/imgui_tables.cpp.o" "gcc" "CMakeFiles/SocialRed.dir/third_party/imgui_tables.cpp.o.d"
+  "/home/gabriels/CLionProjects/SocialRed/third_party/imgui_widgets.cpp" "CMakeFiles/SocialRed.dir/third_party/imgui_widgets.cpp.o" "gcc" "CMakeFiles/SocialRed.dir/third_party/imgui_widgets.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/SocialRed.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SocialRed.dir/src/Menu.cpp.o"
+  "CMakeFiles/SocialRed.dir/src/Menu.cpp.o.d"
+  "CMakeFiles/SocialRed.dir/src/RedSocial.cpp.o"
+  "CMakeFiles/SocialRed.dir/src/RedSocial.cpp.o.d"
+  "CMakeFiles/SocialRed.dir/src/main.cpp.o"
+  "CMakeFiles/SocialRed.dir/src/main.cpp.o.d"
+  "CMakeFiles/SocialRed.dir/third_party/imgui.cpp.o"
+  "CMakeFiles/SocialRed.dir/third_party/imgui.cpp.o.d"
+  "CMakeFiles/SocialRed.dir/third_party/imgui_draw.cpp.o"
+  "CMakeFiles/SocialRed.dir/third_party/imgui_draw.cpp.o.d"
+  "CMakeFiles/SocialRed.dir/third_party/imgui_impl_glfw.cpp.o"
+  "CMakeFiles/SocialRed.dir/third_party/imgui_impl_glfw.cpp.o.d"
+  "CMakeFiles/SocialRed.dir/third_party/imgui_impl_opengl3.cpp.o"
+  "CMakeFiles/SocialRed.dir/third_party/imgui_impl_opengl3.cpp.o.d"
+  "CMakeFiles/SocialRed.dir/third_party/imgui_tables.cpp.o"
+  "CMakeFiles/SocialRed.dir/third_party/imgui_tables.cpp.o.d"
+  "CMakeFiles/SocialRed.dir/third_party/imgui_widgets.cpp.o"
+  "CMakeFiles/SocialRed.dir/third_party/imgui_widgets.cpp.o.d"
+  "SocialRed"
+  "SocialRed.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SocialRed.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
